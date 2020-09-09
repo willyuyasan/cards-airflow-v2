0b1f529fbc4ffe0fb42726e4d34043925e851d05
Vikas Malhotra <vmalhotra@redventures.com>
2020-09-09 12:39:03 -0500

    updating the code with tpg tenant and amp page viewed

--- dags/data-lake-dw-cdm-sdk-cards-staging-hourly.py
+++ dags/data-lake-dw-cdm-sdk-cards-staging-hourly.py
@@ -34,7 +34,7 @@ extra_small_m5_xlarge_1w_task_custom_cluster = {
     'spark_conf': {
       'spark.sql.sources.partitionOverwriteMode': 'dynamic',
       'spark.driver.extraJavaOptions': '-Dconfig.resource=application-cards-qa.conf',
-      'spark.databricks.clusterUsageTags.autoTerminationMinutes' : '60'
+      'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
     },
     'spark_env_vars': {
       'java_opts': '-Dconfig.resource=application-cards-qa.conf'
@@ -418,6 +418,20 @@ paidsearch_staging_jar_task = {
     ]
 }
 
+amp_page_viewed_staging_jar_task = {
+    'main_class_name': "com.redventures.cdm.datamart.cards.Runner",
+    'parameters': [
+        "RUN_FREQUENCY="     + "hourly",
+        "START_DATE="        + (datetime.now() - (timedelta(days=int(int(Variable.get("DBX_CCDC_SDK_lookback_days")))))).strftime("%Y-%m-%d"),
+        "END_DATE="          + datetime.now().strftime("%Y-%m-%d"),
+        "TABLES="            + "com.redventures.cdm.cohesion.staging.AmpPageViewed",
+        "ACCOUNT="           + "cards",
+        "READ_BUCKET="       + "rv-core-pipeline",
+        "TENANTS="           + Variable.get("DBX_TPG_Tenant_Id"),
+        "WRITE_BUCKET="      + "rv-core-ccdc-datamart-qa"
+    ]
+}
+
 # DAG Creation Step
 with DAG('data-lake-dw-cdm-sdk-cards-staging-hourly',
          schedule_interval='30 0-5,9-23 * * *',
@@ -576,6 +590,16 @@ with DAG('data-lake-dw-cdm-sdk-cards-staging-hourly',
         polling_period_seconds  =   120
     )
 
+    amp_page_viewed_staging = DatabricksSubmitRunOperator(
+            task_id                = 'amp-page-viewed-staging',
+            new_cluster            = extra_small_m5_xlarge_1w_task_custom_cluster,
+            spark_jar_task         = amp_page_viewed_staging_jar_task,
+            libraries              = staging_libraries,
+            timeout_seconds        = 3600,
+            databricks_conn_id     = airflow_svc_token,
+            polling_period_seconds = 120
+        )
+
     ccdc_staging_tables = DummyOperator(
         task_id='external-ccdc-staging'
     )
@@ -600,7 +624,7 @@ session_staging >> paidsearch_staging
 [ page_view_staging, page_metrics_staging, product_clicked_staging, product_viewed_staging, element_clicked_staging, element_viewed_staging, cookie_identified_staging, field_inputted_staging, device_staging, location_staging, decsion_staging, traffic_sources_staging, form_submitted_staging, paidsearch_staging] >> ccdc_staging_tables
 
 # TPG Staging Dependencies
-[ page_view_staging, page_metrics_staging, product_clicked_staging, product_viewed_staging, element_clicked_staging, element_viewed_staging, cookie_identified_staging, field_inputted_staging, device_staging, location_staging, decsion_staging, traffic_sources_staging, form_submitted_staging, paidsearch_staging] >> tpg_staging_tables
+[ page_view_staging, page_metrics_staging, product_clicked_staging, product_viewed_staging, element_clicked_staging, element_viewed_staging, cookie_identified_staging, field_inputted_staging, device_staging, location_staging, decsion_staging, traffic_sources_staging, form_submitted_staging, paidsearch_staging, amp_page_viewed_staging] >> tpg_staging_tables
 
 # Amex Business Dependencies
 [ page_view_staging, page_metrics_staging, product_clicked_staging, product_viewed_staging, element_clicked_staging, element_viewed_staging, device_staging, location_staging, decsion_staging, traffic_sources_staging, form_submitted_staging, paidsearch_staging] >> amex_business_staging_tables

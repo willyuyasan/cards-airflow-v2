cc6561e60d51f442392e1101c293fae26be0f101
tkanth <53434301+tkanth@users.noreply.github.com>
2020-09-16 09:42:18 -0400

    Feat to Dev:2020-09-16:v1-DEC-667
    Adding HopPageViewed Staging table to hourly dag

--- dags/data-lake-dw-cdm-sdk-cards-staging-hourly.py
+++ dags/data-lake-dw-cdm-sdk-cards-staging-hourly.py
@@ -317,6 +317,22 @@ paidsearch_staging_jar_task = {
     ]
 }
 
+hoppageviewed_staging_jar_task = {
+    'main_class_name': "com.redventures.cdm.datamart.cards.Runner",
+    'parameters': [
+        "RUN_FREQUENCY=" + "hourly",
+        "START_DATE=" + (
+            datetime.now() - (timedelta(days=int(int(Variable.get("DBX_CCDC_SDK_lookback_days")))))).strftime(
+            "%Y-%m-%d"),
+        "END_DATE=" + datetime.now().strftime("%Y-%m-%d"),
+        "TABLES=" + "com.redventures.cdm.datamart.cards.common.staging.HopPageViewed",
+        "ACCOUNT=" + "cards",
+        "READ_BUCKET=" + "rv-core-pipeline",
+        "TENANTS=" + Variable.get("DBX_TPG_CCDC_SDK_Tenants"),
+        "WRITE_BUCKET=" + "rv-core-ccdc-datamart-qa"
+    ]
+}
+
 # DAG Creation Step
 with DAG('data-lake-dw-cdm-sdk-cards-staging-hourly',
          schedule_interval='30 0-5,9-23 * * *',
@@ -484,6 +500,16 @@ with DAG('data-lake-dw-cdm-sdk-cards-staging-hourly',
         databricks_conn_id=airflow_svc_token,
         polling_period_seconds=120
     )
+    
+    hoppageviewed_staging = DatabricksSubmitRunOperator(
+        task_id='paidsearch-staging',
+        new_cluster=extra_small_task_custom_cluster,
+        spark_jar_task=hoppageviewed_staging_jar_task,
+        libraries=staging_libraries,
+        timeout_seconds=3600,
+        databricks_conn_id=airflow_svc_token,
+        polling_period_seconds=120
+    )
 
     ccdc_staging_tables = DummyOperator(
         task_id='external-ccdc-staging'
@@ -508,12 +534,12 @@ session_staging >> paidsearch_staging
 # CCDC Staging Dependencies
 [page_view_staging, page_metrics_staging, product_clicked_staging, product_viewed_staging, element_clicked_staging, element_viewed_staging, cookie_identified_staging,
     field_inputted_staging, device_staging, location_staging, decsion_staging, traffic_sources_staging, form_submitted_staging,
-    paidsearch_staging] >> ccdc_staging_tables
+    paidsearch_staging,hoppageviewed_staging] >> ccdc_staging_tables
 
 # TPG Staging Dependencies
 [page_view_staging, page_metrics_staging, product_clicked_staging, product_viewed_staging, element_clicked_staging, element_viewed_staging, cookie_identified_staging,
     field_inputted_staging, device_staging, location_staging, decsion_staging, traffic_sources_staging, form_submitted_staging, amp_page_viewed_staging,
-    paidsearch_staging] >> tpg_staging_tables
+    paidsearch_staging,hoppageviewed_staging] >> tpg_staging_tables
 
 # Amex Business Dependencies
 [page_view_staging, page_metrics_staging, product_clicked_staging, product_viewed_staging, element_clicked_staging, element_viewed_staging,

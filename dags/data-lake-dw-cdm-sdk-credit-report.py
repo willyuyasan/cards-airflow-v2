2099861654f4e0dfbd3aa4ea1da713f292076b3f
Ritesh Shukla <rshukla@redventures.com>
2020-10-20 12:35:09 -0700

    formatting changes

--- dags/data-lake-dw-cdm-sdk-credit-report.py
+++ dags/data-lake-dw-cdm-sdk-credit-report.py
@@ -43,8 +43,7 @@ small_task_custom_cluster = {
     },
     'spark_env_vars': {
         'java_opts': ['-Dconfig.resource=application-cards-qa.conf',
-                      '-Dcdm.runner.customParameters.dataLakeTempDir=' + Variable.get("DBX_DATALAKE_TEMP_DIR")
-                    ]
+                      '-Dcdm.runner.customParameters.dataLakeTempDir=' + Variable.get("DBX_DATALAKE_TEMP_DIR")]
     },
     "aws_attributes": {
         "availability": "SPOT_WITH_FALLBACK",

ffa859fc2d8f2d76c3fb1c275b57870bcd40b4ba
Ritesh Shukla <rshukla@redventures.com>
2021-01-28 14:42:07 -0800

    Fixed library path

--- dags/data-lake-dw-cdm-sdk-ccdc-reporting-daily.py
+++ dags/data-lake-dw-cdm-sdk-ccdc-reporting-daily.py
@@ -169,7 +169,7 @@ reporting_libraries = [
         "jar": "dbfs:/FileStore/jars/a750569c_d6c0_425b_bf2a_a16d9f05eb25-RedshiftJDBC42_1_2_1_1001-0613f.jar",
     },
     {
-        "jar": "dbfs:/Libraries/JVM/cdm-data-mart-cards/" + Variable.get("environment") + "scala-2.12/cdm-data-mart-cards-assembly-0.0.1-SNAPSHOT.jar",
+        "jar": "dbfs:/Libraries/JVM/cdm-data-mart-cards/" + Variable.get("environment") + "/scala-2.12/cdm-data-mart-cards-assembly-0.0.1-SNAPSHOT.jar",
     }
 ]
 

2b1b186eff2b4473e289006c662facc6f4cabc52
Vikas Malhotra <vmalhotra@redventures.com>
2021-02-22 10:44:33 -0600

    adding in amex backfill dags

---
+++ dags/data-lake-dw-cdm-sdk-amex-consumer-reporting-backfill.py
@@ -0,0 +1,109 @@
+from airflow import DAG
+from airflow.models import Variable
+from datetime import datetime, timedelta
+from airflow.operators import ExternalTaskSensor
+from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
+from operators.finserv_operator import FinServDatabricksSubmitRunOperator
+from rvairflow import slack_hook as sh
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 11, 1),
+    'email': ['vmalhotra@redventures.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'on_failure_callback': sh.slack_failure_callback(slack_connection_id=Variable.get("slack-connection-name")),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    # 'op_kwargs': cfg_dict,
+    'provide_context': True
+}
+
+# token variable
+airflow_svc_token = "databricks_airflow_svc_token"
+ACCOUNT = 'cards'
+DAG_NAME = 'data-lake-dw-cdm-sdk-amex-consumer-reporting-backfill'
+
+LOG_PATH = {
+    'dbfs': {
+        'destination': 'dbfs:/tmp/airflow_logs/%s/%s/%s/%s' % (ACCOUNT, Variable.get("environment"), DAG_NAME, datetime.date(datetime.now()))
+    }
+}
+
+medium_task_cluster = {
+    'spark_version': '7.3.x-scala2.12',
+    'node_type_id': Variable.get("DBX_MEDIUM_CLUSTER"),
+    'driver_node_type_id': Variable.get("DBX_MEDIUM_CLUSTER"),
+    'num_workers': Variable.get("DBX_MEDIUM_CLUSTER_NUM_NODES"),
+    'auto_termination_minutes': 0,
+    'dbfs_cluster_log_conf': 'dbfs://home/cluster_log',
+    'spark_conf': {
+        'spark.sql.sources.partitionOverwriteMode': 'dynamic',
+        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE"),
+        'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
+    },
+    'spark_env_vars': {
+        'java_opts': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE")
+    },
+    "aws_attributes": {
+        "availability": "SPOT_WITH_FALLBACK",
+        'ebs_volume_count': 2,
+        'ebs_volume_size': 100,
+        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
+        'first_on_demand': '2',
+        'spot_bid_price_percent': '70',
+        'zone_id': 'us-east-1c',
+        "instance_profile_arn": Variable.get("DBX_AMEX_IAM_ROLE"),
+    },
+    'custom_tags': {
+        'Partner': 'B131',
+        'Project': 'American Express - Consumer',
+        'DagId': "{{ti.dag_id}}",
+        'TaskId': "{{ti.task_id}}"
+    },
+}
+
+reporting_libraries = [
+    {
+        "jar": "dbfs:/FileStore/jars/a750569c_d6c0_425b_bf2a_a16d9f05eb25-RedshiftJDBC42_1_2_1_1001-0613f.jar",
+    },
+    {
+        "jar": "dbfs:/Libraries/JVM/cdm-data-mart-cards/" + Variable.get("environment") + "/scala-2.12/cdm-data-mart-cards-assembly-0.0.1-SNAPSHOT.jar",
+    }
+]
+
+# Reporting table tasks
+conversion_reporting_jar_task = {
+    'main_class_name': "com.redventures.cdm.datamart.cards.Runner",
+    'parameters': [
+        "RUN_FREQUENCY=" + "hourly",
+        "START_DATE=" + (
+            datetime.strptime(Variable.get("AMEX_CONSUMER_BACKFILL_LOOKBACK_TODATE"), "%Y-%m-%d") - (timedelta(days=int(int(Variable.get("AMEX_CONSUMER_BACKFILL_LOOKBACK_DAYS")))))).strftime(
+            "%Y-%m-%d"),
+        "END_DATE=" + Variable.get("AMEX_CONSUMER_BACKFILL_LOOKBACK_TODATE"),
+        "TENANTS=" + Variable.get("DBX_AMEX_CONSUMER_Tenant_Id"),
+        "TABLES=" + "com.redventures.cdm.datamart.cards.amex_consumer.reporting.Conversion",
+        "ACCOUNT=" + Variable.get("DBX_AMEX_CONSUMER_Account"),
+        "WRITE_BUCKET=" + Variable.get("DBX_AMEX_Bucket"),
+        "READ_BUCKET=" + Variable.get("DBX_CARDS_Bucket")
+    ]
+}
+
+# DAG Creation Step
+with DAG('data-lake-dw-cdm-sdk-amex-consumer-reporting-backfill',
+         schedule_interval=None,
+         dagrun_timeout=timedelta(hours=4),
+         catchup=False,
+         max_active_runs=1,
+         default_args=default_args) as dag:
+
+    conversion_reporting = FinServDatabricksSubmitRunOperator(
+        task_id='conversion-reporting',
+        new_cluster=medium_task_cluster,
+        spark_jar_task=conversion_reporting_jar_task,
+        libraries=reporting_libraries,
+        timeout_seconds=14400,
+        databricks_conn_id=airflow_svc_token,
+        polling_period_seconds=240
+    )

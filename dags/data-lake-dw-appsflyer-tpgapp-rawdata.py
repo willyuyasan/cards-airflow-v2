f99711ae7ba61750fe4daf4ebb1474428c6a3578
kbhargavaram-rv <kbhargavaram@redventures.com>
2021-05-18 08:48:20 -0700

    dev changes

--- dags/data-lake-dw-appsflyer-tpgapp-rawdata.py
+++ dags/data-lake-dw-appsflyer-tpgapp-rawdata.py
@@ -49,7 +49,7 @@ def make_request(**kwargs):
 
     try:
         print(cmd)
-        returned_value = subprocess.call(cmd,shell=True)
+        returned_value = subprocess.call(cmd, shell=True)
         returned_value = os.system(cmd)
         print('Returned value:', returned_value)
         print("File copied successfully")

50ae719beff1874feff2ff5d255a4b93b7087ddf
brandonmckay <69610179+brandonmckay@users.noreply.github.com>
2021-05-28 12:02:14 -0400

    removed trailing comma

--- dags/data-lake-dw-cdm-sdk-ccdc-reporting-daily.py
+++ dags/data-lake-dw-cdm-sdk-ccdc-reporting-daily.py
@@ -252,7 +252,7 @@ attribution_reporting_jar_task = {
         "TABLES=" + "com.redventures.cdm.datamart.cards.ccdc.reporting.Attribution",
         "ACCOUNT=" + Variable.get("DBX_CCDC_Account"),
         "WRITE_BUCKET=" + Variable.get("DBX_CCDC_Bucket"),
-        "READ_BUCKET=" + Variable.get("DBX_CARDS_Bucket"),
+        "READ_BUCKET=" + Variable.get("DBX_CARDS_Bucket") #,
         # "CUSTOM_PARAMETERS__redshift_Partitioned_Days=" + Variable.get("DBX_CCDC_Attribution_Redshift_Partitoned_Upload_Days")
     ]
 }

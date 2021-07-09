4f38512c98bfb6940c75a0f1e1c6e0b16e5fa784
mikedey <mdey@redventures.com>
2021-07-09 11:16:25 -0500

    Adding dag to test connectivity to mysql in qa

--- dags/mikes-dag.py
+++ dags/mikes-dag.py
@@ -7,6 +7,8 @@ from airflow.hooks.base_hook import BaseHook
 from airflow.hooks.mysql_hook import MySqlHook
 import csv
 import boto3
+import mysql_to_s3 as dh
+
 
 conn = BaseHook.get_connection("mysql_conn_id")
 BASE_URI = conn.host
@@ -15,20 +17,6 @@ BASE_URI = conn.host
 # api_key = Variable.get("APPSFLYER_API_TOKEN_V1")
 S3_BUCKET = 'cards-de-airflow-logs-qa-us-west-2'
 S3_KEY = 'temp/test_mysql_conn'
-
-
-def my_custom_function(ts, **kwargs):
-    """
-    This can be any python code you want and is called from the python operator.
-    The code is not executed until the task is run by the airflow scheduler.
-    """
-    hook = MySqlHook(mysql_conn_id='mysql_ro_conn')
-    hook.run("select * from information_schema.tables")
-    print('hooked')
-    # hook.run(copy_command)
-    print(f"I am task number {kwargs['task_number']}. This DAG Run execution date is {ts} and the current time is {datetime.now()}")
-    print('Here is the full DAG Run context. It is available because provide_context=True')
-    print(kwargs)
 
 
 def execute(**kwargs):
@@ -48,6 +36,26 @@ def execute(**kwargs):
     print("Loading file into S3")
     with open(file, 'rb') as f:
         response = s3.upload_fileobj(f, S3_BUCKET, 'data-lake/temp/mike_test')
+    print(response)
+
+
+def mysql_table_to_s3(table, bucket, key, **kwargs):
+    s3 = boto3.client('s3')
+    mysql = MySqlHook(mysql_conn_id='mysql_ro_conn')
+    print("Dumping MySQL query results to local file")
+    conn = mysql.get_conn()
+    cursor = conn.cursor()
+    cursor.execute(f'select * from {kwargs["table"]}')
+    file = 'mike_temp.csv'
+    with open(file, 'w', newline='') as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerows(cursor)
+        f.flush()
+        cursor.close()
+        conn.close()
+    print("Loading file into S3")
+    with open(file, 'rb') as f:
+        response = s3.upload_fileobj(f, kwargs['bucket'], kwargs['key'])
     print(response)
 
 
@@ -77,20 +85,11 @@ with DAG('mikes_dag',
 
     # generate tasks with a loop. task_id must be unique
     # for task in range(5):
-    tn = PythonOperator(
-        task_id=f'python_print_date_{0}',
-        python_callable=my_custom_function,  # make sure you don't include the () of the function
-        op_kwargs={'task_number': 0},
-        provide_context=True
-    )
-
-    # generate tasks with a loop. task_id must be unique
-    # for task in range(5):
     tm = PythonOperator(
-        task_id=f'load_mysql',
-        python_callable=execute,  # make sure you don't include the () of the function
-        op_kwargs={'task_number': 0},
+        task_id=f'load_mysql_new',
+        python_callable=mysql_table_to_s3,  # make sure you don't include the () of the function
+        op_kwargs={'bucket': S3_BUCKET, 'key': S3_KEY, 'table': 'information_schema.tables'},
         provide_context=True
     )
 
-    t0 >> [tn, tm]
+    t0 >> tm

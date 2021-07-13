aeebc9995079e54ea18dbb4e511fc2bb55bd092c
rv-rzagade <rzagade@redventures.com>
2021-07-13 19:00:45 -0400

    Dec 926/airflow migration (#374)
    * Adding Merge Example Dag
    
    * adding example dag
    
    * formatting
    
    * SQL cahnge

--- dags/sql/merge/cccom/poc_dim_device_type.sql
+++ dags/sql/merge/cccom/poc_dim_device_type.sql
@@ -1,13 +1,13 @@
 begin transaction;
 
-update cccom_dw.dim_device_types_poc
+update cccom_dw.dim_device_types
 set name = s.name,
 description = s.description,
 load_date = sysdate
 from cccom_dw.stg_device_types s
-where s.device_type_id = dim_device_types_poc.device_type_id;
+where s.device_type_id = dim_device_types.device_type_id;
 
-insert into cccom_dw.dim_device_types_poc(
+insert into cccom_dw.dim_device_types(
 device_type_key,
 source_key,
 device_type_id,
@@ -20,7 +20,7 @@ s.device_type_id,
 s.name,
 s.description
 from cccom_dw.stg_device_types s
-cross join (select nvl(max(device_type_key),100) as last_key from cccom_dw.dim_device_types_poc)
-where s.device_type_id not in (select d.device_type_id from cccom_dw.dim_device_types_poc d);
+cross join (select nvl(max(device_type_key),100) as last_key from cccom_dw.dim_device_types)
+where s.device_type_id not in (select d.device_type_id from cccom_dw.dim_device_types d);
 
 end transaction;

34f5d4958505753602280a0bebdd37d9cb9b63cf
rv-rzagade <rzagade@redventures.com>
2021-07-13 15:50:43 -0400

    Dec 926/airflow migration (#373)
    * Adding Merge Example Dag
    
    * adding example dag
    
    * formatting

---
+++ dags/sql/merge/cccom/poc_dim_device_type.sql
@@ -0,0 +1,26 @@
+begin transaction;
+
+update cccom_dw.dim_device_types_poc
+set name = s.name,
+description = s.description,
+load_date = sysdate
+from cccom_dw.stg_device_types s
+where s.device_type_id = dim_device_types_poc.device_type_id;
+
+insert into cccom_dw.dim_device_types_poc(
+device_type_key,
+source_key,
+device_type_id,
+name,
+description
+)
+select last_key + row_number() over (order by s.device_type_id),
+1,
+s.device_type_id,
+s.name,
+s.description
+from cccom_dw.stg_device_types s
+cross join (select nvl(max(device_type_key),100) as last_key from cccom_dw.dim_device_types_poc)
+where s.device_type_id not in (select d.device_type_id from cccom_dw.dim_device_types_poc d);
+
+end transaction;

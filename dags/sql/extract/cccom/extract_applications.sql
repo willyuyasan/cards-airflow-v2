976a7c5908ef0ca4d5d6f00d58f071c85520886b
rv-rzagade <rzagade@redventures.com>
2021-07-20 23:19:50 -0400

    Dec 926/airflow migration3 (#419)
    * Adding new Dags
    
    * Adding new dag
    
    * Adding new dag to QA
    
    * Adding new DAG to QA
    
    * Adding new DAG
    
    * Adding new DAG
    
    * Adding new DAG
    
    * Changing where condition for testing

--- dags/sql/extract/cccom/extract_applications.sql
+++ dags/sql/extract/cccom/extract_applications.sql
@@ -5,7 +5,9 @@ ifnull(submission_date,'') as submission_date,
 ifnull(state,'') as state,
 last_updated
 from cccomus.applications
-where created_date >= ( last_day(now()) + interval 1 day - interval 12 month )
-  and submission_date < '2019-02-01';
+where 1 = 1
+--and created_date >= ( last_day(now()) + interval 1 day - interval 12 month )
+and created_date >= ( last_day(now()) + interval 1 day - interval 72 month )
+and submission_date < '2019-02-01';
 
 #TODO: incremental extract on submission_date and last_updated

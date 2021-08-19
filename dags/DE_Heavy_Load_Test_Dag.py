f3a7f03bfb72a8a9743fdc1be2f7e9347a89b524
rv-rzagade <rzagade@redventures.com>
2021-08-19 19:09:13 -0400

    Adding Compres True to test heavy file (#579)

--- dags/DE_Heavy_Load_Test_Dag.py
+++ dags/DE_Heavy_Load_Test_Dag.py
@@ -32,8 +32,7 @@ with DAG('DE_Heavy_Load_Test_Dag',
     test_extract_heavy_data_to_s3 = PythonOperator(
         task_id='test-extract-heay-data-to-s3',
         python_callable=mysql_table_to_s3,
-        op_kwargs={'extract_script': 'cccom/test_extract_transaction_data_heavy.sql',
-                   'key': 'test_extract_transaction_data_heavy.csv'},
+        op_kwargs={'extract_script': 'cccom/test_extract_transaction_data_heavy.sql', 'key': 'test_extract_transaction_data_heavy.csv', 'compress': True},
         provide_context=True
     )
 test_extract_heavy_data_to_s3

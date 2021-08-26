437131545470fbfd8353a8717421d40c47a882c5
rv-rzagade <rzagade@redventures.com>
2021-08-26 01:02:24 -0400

    Dec 986/airflow migration 0826 01 (#632)
    * Adding test dag to Develop for branch
    
    * Addinv env to bash operator
    
    * Addin venv to Bash operator
    
    * adding one more bash operator to dag
    
    * adding shell for test
    
    * Adding Shell for testing
    
    * adding dag for testing
    
    * Adding echo to test
    
    * Adding pwd

--- dags/scripts/shell/cccom-arp-pg-refresh-dim-tables.sh
+++ dags/scripts/shell/cccom-arp-pg-refresh-dim-tables.sh
@@ -10,7 +10,7 @@ echo ${MYSQL_DBHOST}
 
 SQL="select concat('select affiliate_reporting.proc_dim_affiliates_refresh(''', a.affiliate_id , ''',''',replace(a.company_name,'''',''''''),''',''',a.email,''',''',replace(a.first_name,'''',''''''),''',''',replace(a.last_name,'''',''''''),''',''',a.status,''',',(case when a.in_house then '''True''' else '''False''' end ),',''',a.time_modified,''',''', a.time_inserted,''');') from cccomus.partner_affiliates a where a.deleted = 0 order by a.time_inserted desc;"
 
-mysql -u ${MYSQL_DB_USER} -p ${MYSQL_DB_PASS} -h ${MYSQL_DBHOST} -AN -e"{SQL}" > ${DUMP_FILEPATH}/arp_dim_refresh.sql
+mysql -u ${MYSQL_DB_USER} -p${MYSQL_DB_PASS} -h ${MYSQL_DBHOST} -AN -e"{SQL}" > ${DUMP_FILEPATH}/arp_dim_refresh.sql
 
 # connect to Postgresql to load the data dumped in the previous step
 export PGPASSWORD= ${PGSQL_DB_PASS}
@@ -21,7 +21,7 @@ rm ${DUMP_FILEPATH}/arp_dim_refresh.sql
 
 SQL="select concat('select affiliate_reporting.proc_dim_websites_refresh(',a.website_id,',''',a.affiliate_id,''',''', replace(a.url,'''','''''') /*replace(replace(replace(a.url,'''',''''''),'=\\','=\\\\'),'/?','//?')*/, ''',''', a.status, ''',''',now(),''',''', now(),''');') from cccomus.partner_websites a where a.deleted = 0;"
 
-mysql -u ${MYSQL_DB_USER} -p ${MYSQL_DB_PASS} -h ${MYSQL_DBHOST} -AN -e"{SQL}" > ${DUMP_FILEPATH}/arp_dim_refresh.sql
+mysql -u ${MYSQL_DB_USER} -p${MYSQL_DB_PASS} -h ${MYSQL_DBHOST} -AN -e"{SQL}" > ${DUMP_FILEPATH}/arp_dim_refresh.sql
 
 # connect to Postgresql to load the data dumped in the previous step
 export PGPASSWORD= ${PGSQL_DB_PASS}
@@ -32,7 +32,7 @@ rm ${DUMP_FILEPATH}/arp_dim_refresh.sql
 
 SQL="select concat('select affiliate_reporting.proc_dim_categories_refresh(', a.page_id,',''',replace(a.page_name,'''',''''''),''',''', a.insert_time,''',''', now(),''');') from cccomus.pages a where a.deleted = 0;"
 
-mysql -u ${MYSQL_DB_USER} -p ${MYSQL_DB_PASS} -h ${MYSQL_DBHOST} -AN -e"{SQL}" > ${DUMP_FILEPATH}/arp_dim_refresh.sql
+mysql -u ${MYSQL_DB_USER} -p${MYSQL_DB_PASS} -h ${MYSQL_DBHOST} -AN -e"{SQL}" > ${DUMP_FILEPATH}/arp_dim_refresh.sql
 
 # connect to Postgresql to load the data dumped in the previous step
 export PGPASSWORD= ${PGSQL_DB_PASS}
@@ -44,3 +44,4 @@ rm ${DUMP_FILEPATH}/arp_dim_refresh.sql
 rm ${DUMP_FILEPATH}/arp_dim_affiliates_refresh_out.log
 rm ${DUMP_FILEPATH}/arp_dim_websites_refresh_out.log
 rm ${DUMP_FILEPATH}/arp_dim_categories_refresh_out.log
+

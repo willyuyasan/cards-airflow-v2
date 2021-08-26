c770b2b16ed13d5748eafd5e79d03bffee9f5fdd
rv-rzagade <rzagade@redventures.com>
2021-08-25 23:20:16 -0400

    Dec 986/airflow migration 0826 01 (#628)
    * Adding test dag to Develop for branch
    
    * Addinv env to bash operator
    
    * Addin venv to Bash operator
    
    * adding one more bash operator to dag
    
    * adding shell for test

--- dags/scripts/shell/cccom-arp-pg-refresh-dim-tables.sh
+++ dags/scripts/shell/cccom-arp-pg-refresh-dim-tables.sh
@@ -5,38 +5,38 @@
 # connect to Mysql to get a dump of the data
 SQL="select concat('select affiliate_reporting.proc_dim_affiliates_refresh(''', a.affiliate_id , ''',''',replace(a.company_name,'''',''''''),''',''',a.email,''',''',replace(a.first_name,'''',''''''),''',''',replace(a.last_name,'''',''''''),''',''',a.status,''',',(case when a.in_house then '''True''' else '''False''' end ),',''',a.time_modified,''',''', a.time_inserted,''');') from cccomus.partner_affiliates a where a.deleted = 0 order by a.time_inserted desc;"
 
-mysql -u {{params.MYSQL_DB_USER}} -p {{params.MYSQL_DB_PASS}} -h {{params.MYSQL_DBHOST}} -AN -e"{SQL}" > {{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
+mysql -u ${{params.MYSQL_DB_USER}} -p ${{params.MYSQL_DB_PASS}} -h ${{params.MYSQL_DBHOST}} -AN -e"{SQL}" > ${{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
 
 # connect to Postgresql to load the data dumped in the previous step
-export PGPASSWORD={{params.PGSQL_DB_PASS}}
-psql -h {{params.PGSQL_DBHOST}} -d {{params.PGSQL_DB}} -U {{params.PGSQL_DB_USER}} -f {{params.DUMP_FILEPATH}}/arp_dim_refresh.sql -o {{params.DUMP_FILEPATH}}/arp_dim_affiliates_refresh_out.log
+export PGPASSWORD= ${{params.PGSQL_DB_PASS}}
+psql -h ${{params.PGSQL_DBHOST}} -d ${{params.PGSQL_DB}} -u ${{params.PGSQL_DB_USER}} -f ${{params.DUMP_FILEPATH}}/arp_dim_refresh.sql -o ${{params.DUMP_FILEPATH}}/arp_dim_affiliates_refresh_out.log
 
-rm {{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
+rm ${{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
 
 
 SQL="select concat('select affiliate_reporting.proc_dim_websites_refresh(',a.website_id,',''',a.affiliate_id,''',''', replace(a.url,'''','''''') /*replace(replace(replace(a.url,'''',''''''),'=\\','=\\\\'),'/?','//?')*/, ''',''', a.status, ''',''',now(),''',''', now(),''');') from cccomus.partner_websites a where a.deleted = 0;"
 
-mysql -u {{params.MYSQL_DB_USER}} -p {{params.MYSQL_DB_PASS}} -h {{params.MYSQL_DBHOST}} -AN -e"{SQL}" > {{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
+mysql -u ${{params.MYSQL_DB_USER}} -p ${{params.MYSQL_DB_PASS}} -h ${{params.MYSQL_DBHOST}} -AN -e"{SQL}" > ${{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
 
 # connect to Postgresql to load the data dumped in the previous step
-export PGPASSWORD={{params.PGSQL_DB_PASS}}
-psql -h {{params.PGSQL_DBHOST}} -d {{params.PGSQL_DB}} -U {{params.PGSQL_DB_USER}} -f {{params.DUMP_FILEPATH}}/arp_dim_refresh.sql -o {{params.DUMP_FILEPATH}}/arp_dim_websites_refresh_out.log
+export PGPASSWORD= ${{params.PGSQL_DB_PASS}}
+psql -h ${{params.PGSQL_DBHOST}} -d ${{params.PGSQL_DB}} -u ${{params.PGSQL_DB_USER}} -f ${{params.DUMP_FILEPATH}}/arp_dim_refresh.sql -o ${{params.DUMP_FILEPATH}}/arp_dim_websites_refresh_out.log
 
-rm {{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
+rm ${{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
 
 
 SQL="select concat('select affiliate_reporting.proc_dim_categories_refresh(', a.page_id,',''',replace(a.page_name,'''',''''''),''',''', a.insert_time,''',''', now(),''');') from cccomus.pages a where a.deleted = 0;"
 
-mysql -u {{params.MYSQL_DB_USER}} -p {{params.MYSQL_DB_PASS}} -h {{params.MYSQL_DBHOST}} -AN -e"{SQL}" > {{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
+mysql -u ${{params.MYSQL_DB_USER}} -p ${{params.MYSQL_DB_PASS}} -h ${{params.MYSQL_DBHOST}} -AN -e"{SQL}" > ${{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
 
 # connect to Postgresql to load the data dumped in the previous step
-export PGPASSWORD={{params.PGSQL_DB_PASS}}
-psql -h {{params.PGSQL_DBHOST}} -d {{params.PGSQL_DB}} -U {{params.PGSQL_DB_USER}} -f {{params.DUMP_FILEPATH}}/arp_dim_refresh.sql -o {{params.DUMP_FILEPATH}}/arp_dim_categories_refresh_out.log
+export PGPASSWORD= ${{params.PGSQL_DB_PASS}}
+psql -h ${{params.PGSQL_DBHOST}} -d ${{params.PGSQL_DB}} -u ${{params.PGSQL_DB_USER}} -f ${{params.DUMP_FILEPATH}}/arp_dim_refresh.sql -o ${{params.DUMP_FILEPATH}}/arp_dim_categories_refresh_out.log
 
-rm {{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
+rm ${{params.DUMP_FILEPATH}}/arp_dim_refresh.sql
 
 # housekeeping
-rm {{params.DUMP_FILEPATH}}/arp_dim_affiliates_refresh_out.log
-rm {{params.DUMP_FILEPATH}}/arp_dim_websites_refresh_out.log
-rm {{params.DUMP_FILEPATH}}/arp_dim_categories_refresh_out.log
+rm ${{params.DUMP_FILEPATH}}/arp_dim_affiliates_refresh_out.log
+rm ${{params.DUMP_FILEPATH}}/arp_dim_websites_refresh_out.log
+rm ${{params.DUMP_FILEPATH}}/arp_dim_categories_refresh_out.log
 

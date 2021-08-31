725f41d60e46319a273465cb1ea5bccab14b2523
lucazwicky <51165295+lucazwicky@users.noreply.github.com>
2021-08-31 13:40:05 -0600

    Update data-lake-dw-cdm-sdk-cards-staging-daily.py

--- dags/data-lake-dw-cdm-sdk-cards-staging-daily.py
+++ dags/data-lake-dw-cdm-sdk-cards-staging-daily.py
@@ -215,7 +215,7 @@ page_view_staging_cof_jar_task = {
     'parameters': [
         "RUN_FREQUENCY=" + "hourly",
         "START_DATE=" + (
-            datetime.now() - (timedelta(days=int(int(Variable.get("DBX_SDK_Daily_Lookback_Days")))))).strftime(
+            datetime.now() - (timedelta(days=2)).strftime(
             "%Y-%m-%d"),
         "END_DATE=" + datetime.now().strftime("%Y-%m-%d"),
         "TABLES=" + "com.redventures.cdm.cohesion.staging.PageView",

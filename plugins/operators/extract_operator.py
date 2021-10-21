db98303eb26a3776e703cd7009218ed2bdb415b9
rv-rzagade <rzagade@redventures.com>
2021-10-20 20:26:59 -0400

    changing operator to user scratch sapce (#747)

--- plugins/operators/extract_operator.py
+++ plugins/operators/extract_operator.py
@@ -49,7 +49,7 @@ def make_request(**kwargs):
 
 
 def compressed_file(cursor, kwargs):
-    with NamedTemporaryFile('wb+') as temp_file:
+    with NamedTemporaryFile(mode='wb+', dir="/scratch") as temp_file:
         with gzip.GzipFile(fileobj=temp_file, mode='a') as gz:
             print('Writing data to gzipped file.')
             for row in cursor:
@@ -95,7 +95,8 @@ def mysql_table_to_s3(**kwargs):
         conn.close()
     else:
         ts = str(time.time()).replace('.', '_')
-        outfile = f'/home/airflow/mysql_{ts}.csv'
+        # outfile = f'/home/airflow/mysql_{ts}.csv'
+        outfile = f'/scratch/mysql_{ts}.csv'
         with open(outfile, 'w', newline='') as f:
             csv_writer = csv.writer(f)
             csv_writer.writerows(cursor)
@@ -129,7 +130,8 @@ def pgsql_table_to_s3(**kwargs):
         conn.close()
     else:
         ts = str(time.time()).replace('.', '_')
-        outfile = f'/home/airflow/pgsql_{ts}.csv'
+        # outfile = f'/home/airflow/pgsql_{ts}.csv'
+        outfile = f'/scratch/pgsql_{ts}.csv'
         with open(outfile, 'w', newline='') as f:
             csv_writer = csv.writer(f)
             csv_writer.writerows(cursor)
@@ -195,7 +197,8 @@ def s3_to_mysql(**kwargs):
     mysql_op = S3ToMySqlOperator(
         s3_source_key=f's3://{S3_BUCKET}/{S3_KEY}',
         mysql_table=sch_tbl,
-        finserv_local_path='/home/airflow',
+        # finserv_local_path='/home/airflow',
+        finserv_local_path='/scratch',
         mysql_duplicate_key_handling=dup_handle if dup_handle else 'IGNORE',
         mysql_extra_options="""
             FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'

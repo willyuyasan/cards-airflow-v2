6294b2b04bd1690a86b879d74560f8ec19920925
rv-rzagade <rzagade@redventures.com>
2021-10-29 17:23:21 -0400

    adding sudo for password. Prod Testing (#772)

--- dags/scripts/shell/cccom-pg-payman-stg-refresh.sh
+++ dags/scripts/shell/cccom-pg-payman-stg-refresh.sh
@@ -10,16 +10,14 @@ if [ {{params.refresh}} == 'dev' ] || [ {{params.refresh}} == 'qa' ]
 then
   exit 0
 fi
-
-
 SQL="select array_to_string(array_agg('-T '||schemaname||'.'||tablename order by tablename ASC), ' ') from pg_tables where schemaname = '${SCHEMATODUMP}'"
 SQL="${SQL} and ( tablename in ( 'flyway_schema_history', 'schema_version' ) or tablename like '%old%' or tablename like '%2019%' or"
 SQL="${SQL} tablename like '%2020%' or tablename like '%2021%' or tablename like '%2bd%' ) "
 SQL="${SQL} group by schemaname;"
-echo "====execute -1 ==="
-# EXTBLIST= `PGPASSWORD=${PAYMANPASS} psql -U ${DBUSER} --host {{params.dbhost}} --port 5432 -d {{params.db}} -t -c "${SQL}"`
-EXTBLIST= psql -U ${DBUSER} --host {{params.dbhost}} --port 5432 -d {{params.db}} -t -c "${SQL}"
-echo "end - 1"
+echo "====execute1 ==="
+EXTBLIST=`sudo PGPASSWORD="${PAYMANPASS}" psql -U ${DBUSER} --host {{params.dbhost}} --port 5432 -d {{params.db}} -t -c "${SQL}"`
+#  echo $PGPASSWORD
+echo "end"
 # prep the list of table to be refreshed and that list will be used for Truncate , drop FK and add FK steps
 SQL="select array_to_string(array_agg(''''||tablename||'''' order by tablename ASC), ', ') from pg_tables where schemaname = '${SCHEMATODUMP}'"
 SQL="${SQL} and ( tablename not in ( 'flyway_schema_history', 'schema_version' ) and tablename not like '%old%' and tablename not like '%2019%' and"

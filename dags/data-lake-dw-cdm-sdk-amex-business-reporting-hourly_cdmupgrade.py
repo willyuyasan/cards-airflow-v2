d06ed81a828cfcfd7c2b803e7fb0700b06d95343
rv-rzagade <rzagade@redventures.com>
2021-11-15 12:57:32 -0500

    Adding PGSQL connection debug to PROD (#806)
    * added the new jar to test the outcometracked_msg_outcomemetadata class
    
    * added the assembly jar new
    
    * added new job to test amex reporting
    
    * added the dag name
    
    * removed the max length from the redshift table
    
    * added the dag to test the amex business
    
    * added the jar and dag for tpg app reporting
    
    * adding the reporting job
    
    * added the new dag for testing expertflyer
    
    * Adding Connection Debug to verify on prod (#805)
    
    * add prosper card
    
    Co-authored-by: rv-lbada <lbada@redventures.com>
    Co-authored-by: rv-lbada <49567629+rv-lbada@users.noreply.github.com>
    Co-authored-by: Gibson Bennett <gbennett@redventures.com>
    Co-authored-by: Gibson Bennett <42449745+g-benn-rv@users.noreply.github.com>

---
+++ dags/data-lake-dw-cdm-sdk-amex-business-reporting-hourly_cdmupgrade.py
@@ -0,0 +1,316 @@
+from airflow import DAG
+from airflow.models import Variable
+from datetime import datetime, timedelta
+from airflow.operators import ExternalTaskSensor
+from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
+from operators.finserv_operator import FinServDatabricksSubmitRunOperator
+from rvairflow import slack_hook as sh
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 11, 1),
+    'email': ['vmalhotra@redventures.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'on_failure_callback': sh.slack_failure_callback(slack_connection_id=Variable.get("slack-connection-travel-name")),
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+    # 'op_kwargs': cfg_dict,
+    'provide_context': True,
+    'cluster_permissions': Variable.get("DE_DBX_CLUSTER_PERMISSIONS")
+}
+
+# token variable
+airflow_svc_token = "databricks_airflow_svc_token"
+ACCOUNT = 'cards'
+DAG_NAME = 'data-lake-dw-cdm-sdk-amex-business-reporting-hourly_cdmupgrade'
+
+LOG_PATH = {
+    'dbfs': {
+        'destination': 'dbfs:/tmp/airflow_logs/%s/%s/%s/%s' % (ACCOUNT, Variable.get("environment"), DAG_NAME, datetime.date(datetime.now()))
+    }
+}
+
+# Cluster Setup Step
+small_task_cluster = {
+    'spark_version': '5.3.x-scala2.11',
+    'node_type_id': Variable.get("DBX_SMALL_CLUSTER"),
+    'driver_node_type_id': Variable.get("DBX_SMALL_CLUSTER"),
+    'num_workers': Variable.get("DBX_SMALL_CLUSTER_NUM_NODES"),
+    'auto_termination_minutes': 0,
+    'dbfs_cluster_log_conf': 'dbfs://home/cluster_log',
+    'spark_conf': {
+        'spark.sql.sources.partitionOverwriteMode': 'dynamic'
+    },
+    "aws_attributes": {
+        "availability": "SPOT_WITH_FALLBACK",
+        'ebs_volume_count': 2,
+        'ebs_volume_size': 100,
+        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
+        'first_on_demand': '2',
+        'spot_bid_price_percent': '70',
+        'zone_id': 'us-east-1c',
+        "instance_profile_arn": Variable.get("DBX_AMEX_IAM_ROLE"),
+    },
+    'custom_tags': {
+        'Partner': 'B130',
+        'Project': 'American Express - OPEN',
+        'DagId': "{{ti.dag_id}}",
+        'TaskId': "{{ti.task_id}}"
+    },
+}
+
+old_medium_task_cluster = {
+    'spark_version': '5.3.x-scala2.11',
+    'node_type_id': Variable.get("DBX_MEDIUM_CLUSTER"),
+    'driver_node_type_id': Variable.get("DBX_MEDIUM_CLUSTER"),
+    'num_workers': Variable.get("DBX_MEDIUM_CLUSTER_NUM_NODES"),
+    'auto_termination_minutes': 0,
+    'dbfs_cluster_log_conf': 'dbfs://home/cluster_log',
+    'spark_conf': {
+        'spark.sql.sources.partitionOverwriteMode': 'dynamic'
+    },
+    "aws_attributes": {
+        "availability": "SPOT_WITH_FALLBACK",
+        'ebs_volume_count': 2,
+        'ebs_volume_size': 100,
+        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
+        'first_on_demand': '2',
+        'spot_bid_price_percent': '70',
+        'zone_id': 'us-east-1c',
+        "instance_profile_arn": Variable.get("DBX_AMEX_IAM_ROLE"),
+    },
+    'custom_tags': {
+        'Partner': 'B130',
+        'Project': 'American Express - OPEN',
+        'DagId': "{{ti.dag_id}}",
+        'TaskId': "{{ti.task_id}}"
+    },
+}
+
+medium_task_cluster = {
+    'spark_version': '7.3.x-scala2.12',
+    'node_type_id': Variable.get("DBX_MEDIUM_CLUSTER"),
+    'driver_node_type_id': Variable.get("DBX_MEDIUM_CLUSTER"),
+    'num_workers': Variable.get("DBX_MEDIUM_CLUSTER_NUM_NODES"),
+    'auto_termination_minutes': 0,
+    'dbfs_cluster_log_conf': 'dbfs://home/cluster_log',
+    'spark_conf': {
+        'spark.sql.sources.partitionOverwriteMode': 'dynamic',
+        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE"),
+        'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
+    },
+    'spark_env_vars': {
+        'java_opts': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE")
+    },
+    "aws_attributes": {
+        "availability": "SPOT_WITH_FALLBACK",
+        'ebs_volume_count': 2,
+        'ebs_volume_size': 100,
+        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
+        'first_on_demand': '2',
+        'spot_bid_price_percent': '70',
+        'zone_id': 'us-east-1c',
+        "instance_profile_arn": Variable.get("DBX_AMEX_IAM_ROLE"),
+    },
+    'custom_tags': {
+        'Partner': 'B130',
+        'Project': 'American Express - OPEN',
+        'DagId': "{{ti.dag_id}}",
+        'TaskId': "{{ti.task_id}}"
+    },
+}
+
+large_task_cluster = {
+    'spark_version': '7.3.x-scala2.12',
+    'node_type_id': Variable.get("DBX_LARGE_CLUSTER"),
+    'driver_node_type_id': Variable.get("DBX_LARGE_CLUSTER"),
+    'num_workers': Variable.get("DBX_LARGE_CLUSTER_NUM_NODES"),
+    'auto_termination_minutes': 0,
+    'dbfs_cluster_log_conf': 'dbfs://home/cluster_log',
+    'spark_conf': {
+        'spark.sql.sources.partitionOverwriteMode': 'dynamic',
+        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE"),
+        'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
+    },
+    'spark_env_vars': {
+        'java_opts': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE")
+    },
+    "aws_attributes": {
+        "availability": "SPOT_WITH_FALLBACK",
+        'ebs_volume_count': 2,
+        'ebs_volume_size': 100,
+        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
+        'first_on_demand': '2',
+        'spot_bid_price_percent': '70',
+        'zone_id': 'us-east-1c',
+        "instance_profile_arn": Variable.get("DBX_AMEX_IAM_ROLE"),
+    },
+    'custom_tags': {
+        'Partner': 'B130',
+        'Project': 'American Express - OPEN',
+        'DagId': "{{ti.dag_id}}",
+        'TaskId': "{{ti.task_id}}"
+    },
+}
+
+base_params_reporting = {
+    "lookBackDays": Variable.get("AMEX_BUSINESS_REPORTING_SHORT_LOOKBACK_DAYS"),
+    "environment": "reporting",
+    "stagingPath": Variable.get("DBX_CARDS_Staging_Path"),
+    "reportingPath": Variable.get("DBX_AMEX_BUSINESS_Reporting_Path"),
+    "dimensionPath": Variable.get("DBX_AMEX_BUSINESS_Dimensions_Path"),
+    "loggingPath": Variable.get("DBX_AMEX_BUSINESS_Logging_Path"),
+    "dataLakePath": Variable.get("DBX_DataLake_Path"),
+    "tenantName": "amex_business",
+    "toDate": "now",
+}
+
+# Libraries
+staging_libraries = [
+    {
+        "jar": "dbfs:/FileStore/jars/a750569c_d6c0_425b_bf2a_a16d9f05eb25-RedshiftJDBC42_1_2_1_1001-0613f.jar",
+    },
+    {
+        "jar": "dbfs:/data-warehouse/production/datawarehouse-builder-0.8.1-tmp.jar",
+    },
+]
+
+reporting_libraries = [
+    {
+        "jar": "dbfs:/FileStore/jars/a750569c_d6c0_425b_bf2a_a16d9f05eb25-RedshiftJDBC42_1_2_1_1001-0613f.jar",
+    },
+    {
+        "jar": "dbfs:/FileStore/jars/93365f5d_f584_4f8e_acbf_b8fec15f6b0b-cdm_data_mart_cards_assembly_0_0_1_SNAPSHOT-97c7e.jar",
+    }
+]
+
+# Reporting table tasks
+conversion_reporting_jar_task = {
+    'main_class_name': "com.redventures.cdm.datamart.cards.Runner",
+    'parameters': [
+        "RUN_FREQUENCY=" + "hourly",
+        "START_DATE=" + (
+            datetime.now() - (timedelta(days=int(int(Variable.get("AMEX_BUSINESS_REPORTING_SHORT_LOOKBACK_DAYS")))))).strftime(
+            "%Y-%m-%d"),
+        "END_DATE=" + datetime.now().strftime("%Y-%m-%d"),
+        "TENANTS=" + Variable.get("DBX_AMEX_BUSINESS_Tenant_Id"),
+        "TABLES=" + "com.redventures.cdm.datamart.cards.amex_business.reporting.Conversion",
+        "ACCOUNT=" + Variable.get("DBX_AMEX_BUSINESS_Account"),
+        "WRITE_BUCKET=" + Variable.get("DBX_AMEX_Bucket"),
+        "READ_BUCKET=" + Variable.get("DBX_CARDS_Bucket")
+    ]
+}
+
+session_reporting_jar_task = {
+    'main_class_name': "com.redventures.cdm.datamart.cards.Runner",
+    'parameters': [
+        "RUN_FREQUENCY=" + "hourly",
+        "START_DATE=" + (
+            datetime.now() - (timedelta(days=int(int(Variable.get("AMEX_BUSINESS_REPORTING_SHORT_LOOKBACK_DAYS")))))).strftime(
+            "%Y-%m-%d"),
+        "END_DATE=" + datetime.now().strftime("%Y-%m-%d"),
+        "TENANTS=" + Variable.get("DBX_AMEX_BUSINESS_Session_Tenant_Id"),
+        "TABLES=" + "com.redventures.cdm.datamart.cards.amex_business.reporting.Session",
+        "ACCOUNT=" + Variable.get("DBX_AMEX_BUSINESS_Account"),
+        "WRITE_BUCKET=" + Variable.get("DBX_AMEX_Bucket"),
+        "READ_BUCKET=" + Variable.get("DBX_CARDS_Bucket")
+    ]
+}
+
+page_view_reporting_jar_task = {
+    'main_class_name': "com.redventures.cdm.datamart.cards.Runner",
+    'parameters': [
+        "RUN_FREQUENCY=" + "hourly",
+        "START_DATE=" + (
+            datetime.now() - (timedelta(days=int(int(Variable.get("AMEX_BUSINESS_REPORTING_SHORT_LOOKBACK_DAYS")))))).strftime(
+            "%Y-%m-%d"),
+        "END_DATE=" + datetime.now().strftime("%Y-%m-%d"),
+        "TENANTS=" + Variable.get("DBX_AMEX_BUSINESS_Tenant_Id"),
+        "TABLES=" + "com.redventures.cdm.datamart.cards.amex_business.reporting.PageView",
+        "ACCOUNT=" + Variable.get("DBX_AMEX_BUSINESS_Account"),
+        "WRITE_BUCKET=" + Variable.get("DBX_AMEX_Bucket"),
+        "READ_BUCKET=" + Variable.get("DBX_CARDS_Bucket")
+    ]
+}
+paid_search_reporting_jar_task = {
+    'main_class_name': "com.redventures.cdm.datamart.cards.Runner",
+    'parameters': [
+        "RUN_FREQUENCY=" + "hourly",
+        "START_DATE=" + (
+            datetime.now() - (timedelta(days=int(int(Variable.get("AMEX_BUSINESS_REPORTING_SHORT_LOOKBACK_DAYS")))))).strftime(
+            "%Y-%m-%d"),
+        "END_DATE=" + datetime.now().strftime("%Y-%m-%d"),
+        "TENANTS=" + Variable.get("DBX_AMEX_BUSINESS_Tenant_Id"),
+        "TABLES=" + "com.redventures.cdm.datamart.cards.common.reporting.PaidSearchSummary",
+        "PAID_SEARCH_COMPANY_ID=" + Variable.get("AMEX_PAIDSEARCH_COMPANY_ID"),
+        "ACCOUNT=" + Variable.get("DBX_AMEX_BUSINESS_Account"),
+        "WRITE_BUCKET=" + Variable.get("DBX_AMEX_Bucket"),
+        "READ_BUCKET=" + "rv-core-pipeline"
+    ]
+}
+
+# DAG Creation Step
+with DAG(DAG_NAME,
+         schedule_interval='0 0-6,11-23 * * *',
+         dagrun_timeout=timedelta(hours=1),
+         catchup=False,
+         max_active_runs=1,
+         default_args=default_args) as dag:
+
+    amex_business_staging_tables = ExternalTaskSensor(
+        task_id='external-amex-business-reporting',
+        external_dag_id='data-lake-dw-cdm-sdk-cards-staging-hourly',
+        external_task_id='external-amex-business-staging',
+        execution_timeout=timedelta(minutes=10),
+        execution_delta=timedelta(minutes=30)
+    )
+
+    conversion_reporting = FinServDatabricksSubmitRunOperator(
+        task_id='conversion-reporting',
+        new_cluster=medium_task_cluster,
+        spark_jar_task=conversion_reporting_jar_task,
+        libraries=reporting_libraries,
+        timeout_seconds=3600,
+        databricks_conn_id=airflow_svc_token,
+        polling_period_seconds=60
+    )
+
+    session_reporting = FinServDatabricksSubmitRunOperator(
+        task_id='session-reporting',
+        new_cluster=large_task_cluster,
+        spark_jar_task=session_reporting_jar_task,
+        libraries=reporting_libraries,
+        timeout_seconds=3600,
+        databricks_conn_id=airflow_svc_token,
+        polling_period_seconds=60
+    )
+
+    page_view_reporting = FinServDatabricksSubmitRunOperator(
+        task_id='page-view-reporting',
+        new_cluster=medium_task_cluster,
+        spark_jar_task=page_view_reporting_jar_task,
+        libraries=reporting_libraries,
+        timeout_seconds=3600,
+        databricks_conn_id=airflow_svc_token,
+        polling_period_seconds=120
+    )
+
+    paid_search_reporting = FinServDatabricksSubmitRunOperator(
+        task_id='paid-search-reporting',
+        new_cluster=medium_task_cluster,
+        spark_jar_task=paid_search_reporting_jar_task,
+        libraries=reporting_libraries,
+        timeout_seconds=3600,
+        databricks_conn_id=airflow_svc_token,
+        polling_period_seconds=120
+    )
+
+
+# Dependencies
+amex_business_staging_tables >> [conversion_reporting, page_view_reporting, session_reporting]
+
+# Defining additional reporting dependencies
+session_reporting >> paid_search_reporting
+conversion_reporting >> paid_search_reporting
---
+++ dags/data-lake-dw-cdm-sdk-ef-adhoc-hourly_cdmupgrade.py
@@ -0,0 +1,233 @@
+from airflow import DAG
+from airflow.models import Variable
+from datetime import datetime, timedelta
+from airflow.operators import ExternalTaskSensor
+from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
+from operators.finserv_operator import FinServDatabricksSubmitRunOperator
+from rvairflow import slack_hook as sh
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 11, 1),
+    'email': ['vmalhotra@redventures.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'on_failure_callback': sh.slack_failure_callback(slack_connection_id=Variable.get("slack-connection-travel-name")),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    # 'op_kwargs': cfg_dict,
+    'provide_context': True,
+    'cluster_permissions': Variable.get("DE_DBX_CLUSTER_PERMISSIONS")
+}
+
+# token variable
+airflow_svc_token = "databricks_airflow_svc_token"
+ACCOUNT = 'cards'
+DAG_NAME = 'data-lake-dw-cdm-sdk-ef-adhoc-hourly_cdmupgrade'
+
+LOG_PATH = {
+    'dbfs': {
+        'destination': 'dbfs:/tmp/airflow_logs/%s/%s/%s/%s' % (ACCOUNT, Variable.get("environment"), DAG_NAME, datetime.date(datetime.now()))
+    }
+}
+
+# Cluster Setup Step
+small_task_cluster = {
+    'spark_version': '7.3.x-scala2.12',
+    'node_type_id': Variable.get("DBX_SMALL_CLUSTER"),
+    'driver_node_type_id': Variable.get("DBX_SMALL_CLUSTER"),
+    'num_workers': Variable.get("DBX_SMALL_CLUSTER_NUM_NODES"),
+    'auto_termination_minutes': 0,
+    'cluster_log_conf': LOG_PATH,
+    'spark_conf': {
+        'spark.sql.sources.partitionOverwriteMode': 'dynamic',
+        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE"),
+        'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
+    },
+    'spark_env_vars': {
+        'java_opts': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE")
+    },
+    "aws_attributes": {
+        "availability": "SPOT_WITH_FALLBACK",
+        'ebs_volume_count': 2,
+        'ebs_volume_size': 100,
+        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
+        'first_on_demand': '2',
+        'spot_bid_price_percent': '70',
+        'zone_id': 'us-east-1c',
+        "instance_profile_arn": Variable.get("DBX_CARDS_IAM_ROLE"),
+    },
+    'custom_tags': {
+        'Partner': 'B814',
+        'Project': 'Cards Allocation',
+        'DagId': "{{ti.dag_id}}",
+        'TaskId': "{{ti.task_id}}"
+    },
+}
+
+medium_task_cluster = {
+    'spark_version': '7.3.x-scala2.12',
+    'node_type_id': Variable.get("DBX_MEDIUM_CLUSTER"),
+    'driver_node_type_id': Variable.get("DBX_MEDIUM_CLUSTER"),
+    'num_workers': Variable.get("DBX_MEDIUM_CLUSTER_NUM_NODES"),
+    'auto_termination_minutes': 0,
+    'cluster_log_conf': LOG_PATH,
+    'spark_conf': {
+        'spark.sql.sources.partitionOverwriteMode': 'dynamic',
+        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE"),
+        'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
+    },
+    'spark_env_vars': {
+        'java_opts': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE")
+    },
+    "aws_attributes": {
+        "availability": "SPOT_WITH_FALLBACK",
+        'ebs_volume_count': 2,
+        'ebs_volume_size': 100,
+        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
+        'first_on_demand': '2',
+        'spot_bid_price_percent': '70',
+        'zone_id': 'us-east-1c',
+        "instance_profile_arn": Variable.get("DBX_CARDS_IAM_ROLE"),
+    },
+    'custom_tags': {
+        'Partner': 'B814',
+        'Project': 'Cards Allocation',
+        'DagId': "{{ti.dag_id}}",
+        'TaskId': "{{ti.task_id}}"
+    },
+}
+
+large_task_cluster = {
+    'spark_version': '7.3.x-scala2.12',
+    'node_type_id': Variable.get("DBX_LARGE_CLUSTER"),
+    'driver_node_type_id': Variable.get("DBX_LARGE_CLUSTER"),
+    'num_workers': 4,
+    'auto_termination_minutes': 0,
+    'cluster_log_conf': LOG_PATH,
+    'spark_conf': {
+        'spark.sql.sources.partitionOverwriteMode': 'dynamic',
+        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE"),
+        'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
+    },
+    'spark_env_vars': {
+        'java_opts': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE")
+    },
+    "aws_attributes": {
+        "availability": "SPOT_WITH_FALLBACK",
+        'ebs_volume_count': 2,
+        'ebs_volume_size': 100,
+        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
+        'first_on_demand': '2',
+        'spot_bid_price_percent': '70',
+        'zone_id': 'us-east-1c',
+        "instance_profile_arn": Variable.get("DBX_CARDS_IAM_ROLE"),
+    },
+    'custom_tags': {
+        'Partner': 'B814',
+        'Project': 'Cards Allocation',
+        'DagId': "{{ti.dag_id}}",
+        'TaskId': "{{ti.task_id}}"
+    },
+}
+
+reporting_libraries = [
+    {
+        "jar": "dbfs:/FileStore/jars/a750569c_d6c0_425b_bf2a_a16d9f05eb25-RedshiftJDBC42_1_2_1_1001-0613f.jar",
+    },
+    {
+        "jar": "dbfs:/FileStore/jars/93365f5d_f584_4f8e_acbf_b8fec15f6b0b-cdm_data_mart_cards_assembly_0_0_1_SNAPSHOT-97c7e.jar",
+    }
+]
+
+# Reporting table tasks
+usage_report_created_alert_jar_task = {
+    'main_class_name': "com.redventures.cdm.datamart.cards.Runner",
+    'parameters': [
+        "RUN_FREQUENCY=" + "daily",
+        "START_DATE=" + (
+            datetime.now() - (timedelta(days=int(int(100))))).strftime(
+            "%Y-%m-%d"),
+        "END_DATE=" + datetime.now().strftime("%Y-%m-%d"),
+        "TENANTS=" + Variable.get("DBX_EF_Tenant_Id"),
+        "TABLES=" + "com.redventures.cdm.datamart.cards.expert_flyer.reporting.AdHocUsageReportCreatedAlert",
+        "ACCOUNT=" + "cards",
+        "WRITE_BUCKET=" + Variable.get("DBX_CARDS_Bucket"),
+        "READ_BUCKET=" + "rv-core-pipeline"
+    ]
+}
+
+usage_report_jar_task = {
+    'main_class_name': "com.redventures.cdm.datamart.cards.Runner",
+    'parameters': [
+        "RUN_FREQUENCY=" + "daily",
+        "START_DATE=" + (
+            datetime.now() - (timedelta(days=int(int(100))))).strftime(
+            "%Y-%m-%d"),
+        "END_DATE=" + datetime.now().strftime("%Y-%m-%d"),
+        "TENANTS=" + Variable.get("DBX_EF_Tenant_Id"),
+        "TABLES=" + "com.redventures.cdm.datamart.cards.expert_flyer.reporting.AdHocUsageReport",
+        "ACCOUNT=" + "cards",
+        "WRITE_BUCKET=" + Variable.get("DBX_CARDS_Bucket"),
+        "READ_BUCKET=" + "rv-core-pipeline"
+    ]
+}
+
+top_users_jar_task = {
+    'main_class_name': "com.redventures.cdm.datamart.cards.Runner",
+    'parameters': [
+        "RUN_FREQUENCY=" + "daily",
+        "START_DATE=" + (
+            datetime.now() - (timedelta(days=int(int(100))))).strftime(
+            "%Y-%m-%d"),
+        "END_DATE=" + datetime.now().strftime("%Y-%m-%d"),
+        "TENANTS=" + Variable.get("DBX_EF_Tenant_Id"),
+        "TABLES=" + "com.redventures.cdm.datamart.cards.expert_flyer.reporting.AdHocTopUsers",
+        "ACCOUNT=" + "cards",
+        "WRITE_BUCKET=" + Variable.get("DBX_CARDS_Bucket"),
+        "READ_BUCKET=" + "rv-core-pipeline"
+    ]
+}
+
+# DAG Creation Step
+with DAG(DAG_NAME,
+         schedule_interval='0 0-4,13-23 * * *',
+         dagrun_timeout=timedelta(hours=1),
+         catchup=False,
+         max_active_runs=1,
+         default_args=default_args
+         ) as dag:
+
+    usage_report_created_alert = FinServDatabricksSubmitRunOperator(
+        task_id='usage-report-created-alert',
+        new_cluster=small_task_cluster,
+        spark_jar_task=usage_report_created_alert_jar_task,
+        libraries=reporting_libraries,
+        timeout_seconds=1200,
+        databricks_conn_id=airflow_svc_token,
+        polling_period_seconds=120
+    )
+
+    usage_report = FinServDatabricksSubmitRunOperator(
+        task_id='usage-report',
+        new_cluster=small_task_cluster,
+        spark_jar_task=usage_report_jar_task,
+        libraries=reporting_libraries,
+        timeout_seconds=1200,
+        databricks_conn_id=airflow_svc_token,
+        polling_period_seconds=120
+    )
+
+    top_users = FinServDatabricksSubmitRunOperator(
+        task_id='top-users',
+        new_cluster=small_task_cluster,
+        spark_jar_task=top_users_jar_task,
+        libraries=reporting_libraries,
+        timeout_seconds=1200,
+        databricks_conn_id=airflow_svc_token,
+        polling_period_seconds=120
+    )
+
+# Dependencies
+usage_report_created_alert >> usage_report

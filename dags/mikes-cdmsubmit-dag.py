e8e46738b2390a8acafd55834d2a95e12733f882
mikedey <mdey@redventures.com>
2021-12-03 08:27:08 -0600

    Working cdmsubmit?

--- dags/mikes-cdmsubmit-dag.py
+++ dags/mikes-cdmsubmit-dag.py
@@ -17,12 +17,12 @@ default_args = {
     'email': ['vmalhotra@redventures.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'on_failure_callback': sh.slack_failure_callback(slack_connection_id=Variable.get("slack-connection-name")),
+    'on_failure_callback': sh.slack_failure_callback(slack_connection_id='{{ var.value.slack-connection-name }}'),
     'retries': 2,
     'retry_delay': timedelta(minutes=5),
     # 'op_kwargs': cfg_dict,
     'provide_context': True,
-    'cluster_permissions': Variable.get("DE_DBX_CLUSTER_PERMISSIONS")
+    'cluster_permissions': '{{ var.value.DE_DBX_CLUSTER_PERMISSIONS }}'
 }
 
 # token variable
@@ -33,60 +33,27 @@ DAG_NAME = 'data-lake-dw-cdm-sdk-cards-staging-daily'
 LOG_PATH = {
     'dbfs': {
         'destination': 'dbfs:/tmp/airflow_logs/%s/%s/%s/%s' % (
-            ACCOUNT, Variable.get("environment"), DAG_NAME, datetime.date(datetime.now()))
+            ACCOUNT, '{{ var.value.environment }}', DAG_NAME, datetime.date(datetime.now()))
     }
 }
 
 # Cluster Setup Step
-extra_small_task_custom_cluster = {
-    'spark_version': '7.3.x-scala2.12',
-    'node_type_id': 'm5a.xlarge',
-    'driver_node_type_id': 'm5a.xlarge',
-    'num_workers': 4,
-    'auto_termination_minutes': 0,
-    'cluster_log_conf': LOG_PATH,
-    'spark_conf': {
-        'spark.sql.sources.partitionOverwriteMode': 'dynamic',
-        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE"),
-        'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
-    },
-    'spark_env_vars': {
-        'java_opts': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE")
-    },
-    "aws_attributes": {
-        "availability": "SPOT_WITH_FALLBACK",
-        'ebs_volume_count': 1,
-        'ebs_volume_size': 100,
-        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
-        'first_on_demand': '2',
-        'spot_bid_price_percent': '60',
-        'zone_id': 'us-east-1b',
-        "instance_profile_arn": Variable.get("DBX_CARDS_IAM_ROLE"),
-    },
-    'custom_tags': {
-        'Partner': 'B814',
-        'Project': 'Cards Allocation',
-        'Dag_id': "{{ ti.dag_id }}",
-        'Task_id': "{{ ti.task_id }}"
-    },
-}
-
 small_task_custom_cluster = {
     'spark_version': '7.3.x-scala2.12',
     'node_type_id': 'm5a.xlarge',
     'driver_node_type_id': 'm5a.xlarge',
-    'num_workers': 4,
-    'auto_termination_minutes': 0,
+    # 'num_workers': 4,
+    # 'auto_termination_minutes': 0,
     'cluster_log_conf': LOG_PATH,
     'spark_conf': {
         'spark.sql.sources.partitionOverwriteMode': 'dynamic',
-        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE"),
+        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + '{{ var.value.SDK_CONFIG_FILE }}',
         'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
     },
     'spark_env_vars': {
-        'java_opts': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE")
+        'java_opts': '-Dconfig.resource=' + '{{ var.value.SDK_CONFIG_FILE }}'
     },
-    "aws_attributes": {
+    # "aws_attributes": {
         "availability": "SPOT_WITH_FALLBACK",
         'ebs_volume_count': 1,
         'ebs_volume_size': 100,
@@ -94,81 +61,19 @@ small_task_custom_cluster = {
         'first_on_demand': '2',
         'spot_bid_price_percent': '60',
         'zone_id': 'us-east-1b',
-        "instance_profile_arn": Variable.get("DBX_CARDS_IAM_ROLE"),
-    },
-    'custom_tags': {
-        'Partner': 'B814',
-        'Project': 'Cards Allocation',
-        'Dag_id': "{{ ti.dag_id }}",
-        'Task_id': "{{ ti.task_id }}"
-    },
+        "instance_profile_arn": Variable.get("DBX_CARDS_IAM_ROLE") # "'{{ var.value.DBX_CARDS_IAM_ROLE }}',
+    # },
+    # 'custom_tags': {
+    #     'Partner': 'B814',
+    #     'Project': 'Cards Allocation',
+    #     'Dag_id': "{{ ti.dag_id }}",
+    #     'Task_id': "{{ ti.task_id }}"
+    # },
 }
+ct = ClusterCustomTags(cluster_type="Development", partner="B814", product="DatabricksDevelopment", owner="cdm", created_by='cdm-databricks_svc')
+env = SparkEnvVars(cdm_secret_scope='airflow', api_secret_scope='airflow')
+cluster = NewCluster(num_workers=4, spark_env_obj=env, custom_tags_obj=ct, **small_task_custom_cluster)
 
-medium_task_custom_cluster = {
-    'spark_version': '7.3.x-scala2.12',
-    'node_type_id': 'm5a.2xlarge',
-    'driver_node_type_id': 'm5a.2xlarge',
-    'num_workers': 6,
-    'auto_termination_minutes': 0,
-    'cluster_log_conf': LOG_PATH,
-    'spark_conf': {
-        'spark.sql.sources.partitionOverwriteMode': 'dynamic',
-        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE"),
-        'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
-    },
-    'spark_env_vars': {
-        'java_opts': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE")
-    },
-    "aws_attributes": {
-        "availability": "SPOT_WITH_FALLBACK",
-        'ebs_volume_count': 3,
-        'ebs_volume_size': 100,
-        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
-        'first_on_demand': '2',
-        'spot_bid_price_percent': '60',
-        'zone_id': 'us-east-1b',
-        "instance_profile_arn": Variable.get("DBX_CARDS_IAM_ROLE"),
-    },
-    'custom_tags': {
-        'Partner': 'B814',
-        'Project': 'Cards Allocation',
-        'Dag_id': "{{ ti.dag_id }}",
-        'Task_id': "{{ ti.task_id }}"
-    },
-}
-
-large_task_custom_cluster = {
-    'spark_version': '7.3.x-scala2.12',
-    'node_type_id': 'm5a.4xlarge',
-    'driver_node_type_id': 'm5a.4xlarge',
-    'num_workers': 8,
-    'auto_termination_minutes': 0,
-    'cluster_log_conf': LOG_PATH,
-    'spark_conf': {
-        'spark.sql.sources.partitionOverwriteMode': 'dynamic',
-        'spark.driver.extraJavaOptions': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE"),
-        'spark.databricks.clusterUsageTags.autoTerminationMinutes': '60'
-    },
-    'spark_env_vars': {
-        'java_opts': '-Dconfig.resource=' + Variable.get("SDK_CONFIG_FILE")
-    },
-    "aws_attributes": {
-        "availability": "SPOT_WITH_FALLBACK",
-        'ebs_volume_count': 3,
-        'ebs_volume_size': 100,
-        'ebs_volume_type': 'GENERAL_PURPOSE_SSD',
-        'first_on_demand': '2',
-        'spot_bid_price_percent': '60',
-        'zone_id': 'us-east-1b',
-        "instance_profile_arn": Variable.get("DBX_CARDS_IAM_ROLE"),
-    },
-    'custom_tags': {
-        'Partner': 'B814',
-        'Project': 'Cards Allocation',
-        'Dag_id': "{{ ti.dag_id }}",
-        'Task_id': "{{ ti.task_id }}"
-    },
-}
 
 # Libraries
 staging_libraries = [
@@ -181,21 +86,26 @@ staging_libraries = [
     },
 ]
 
-runner_params = RunnerParams(tenants=Variable.get("DBX_CARDS_SDK_Tenants"),
-                             account=ACCOUNT,
-                             read_bucket='rv-core-pipeline',
-                             write_bucket=Variable.get("DBX_CARDS_Bucket"),
-                             paid_search_company_id=Variable.get("CARDS_PAIDSEARCH_COMPANY_IDS"),
-                             environment=Variable.get("environment"),
-                             custom_parameter__dbx_secrets_scope='airflow')
-
 # Notebook Task Parameter Setup:
 tables = 'com.redventures.cdm.datamart.cards.common.staging.Session'
-session_staging_jar_task = JarTask(cluster=small_task_custom_cluster,
+
+runner_params = RunnerParams(tenants='{{ var.value.DBX_CARDS_SDK_Tenants }}',
+                             account=ACCOUNT,
+                             read_bucket='rv-core-pipeline',
+                             write_bucket='{{ var.value.DBX_CARDS_Bucket }}',
+                             paid_search_company_id='{{ var.value.CARDS_PAIDSEARCH_COMPANY_IDS }}',
+                             environment='{{ var.value.environment }}',
+                             tables=tables,
+                             custom_parameter__dbx_secrets_scope='airflow')
+
+
+session_staging_jar_task = JarTask(cluster=cluster,
                params=runner_params,
                main_class="com.redventures.cdm.datamart.cards.Runner",
                jar_libraries=staging_libraries,
                tables=tables)
+
+
 
 # DAG Creation Step
 with DAG('mikes-cdmsubmit-dag',
@@ -209,13 +119,13 @@ with DAG('mikes-cdmsubmit-dag',
     # 1. Replace task_id with job_name
     # 2. Replace spark_jar_task with task
     # 3.
+    # Optional. convert variable.get() to {{ var.value.<variable_name> }}. Replace Variable.get\(\"(.*?)\"\) with \'\{\{ var.value.$1 \}\}\'
 
     session_staging = CdmDatabricksSubmitRunOperator(
         job_name='session-staging',
         new_cluster=small_task_custom_cluster,
-        # task=session_staging_jar_task,
         task=session_staging_jar_task,
-        # libraries=staging_libraries,
+        libraries=staging_libraries,
         timeout_seconds=3600,
         databricks_conn_id=airflow_svc_token,
         polling_period_seconds=120

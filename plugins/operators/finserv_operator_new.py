a90fe577acef9a9b8ae6fb72f2a35cccf58f64c9
mikedey <mdey@redventures.com>
2021-12-14 15:41:57 -0600

    libraries

--- plugins/operators/finserv_operator_new.py
+++ plugins/operators/finserv_operator_new.py
@@ -109,7 +109,7 @@ class FinServDatabricksSubmitRunOperatorNew(CdmDatabricksSubmitRunOperator):
                        jar_libraries=libraries,
                        tables=spark_jar_params.get('tables'))
 
-        self.libraries=libraries
+        self.libraries = libraries
 
         super(CdmDatabricksSubmitRunOperator, self).__init__(
             job_name=task_id.replace('-', '_'),
